5edabce58225eacc79228116f258d413014f8158
Alberto Hamilton Castro <alberto@isaatc.ull.es>
2018-07-12 13:33:57 +0100

    Limpiamos sesion.js

--- code/portal-vm/sesion.js
+++ code/portal-vm/sesion.js
@@ -1,47 +1,45 @@
-var config = require('./config.json');
+const config = require('./config.json');
 
 module.exports = {
 
-  createsession : function(app, websocket_client){
+  createsession : function(app, websocket_client) {
 
-var session = require("express-session");
-var redis   = require("redis");
-var socketIOSession = require("socket.io.session");
-var redisStore = require('connect-redis')(session);
+    const session = require('express-session');
+    const redis   = require('redis');
+    const socketIOSession = require('socket.io.session');
+    const redisStore = require('connect-redis')(session);
 
-var client  = redis.createClient(6379, config.host_redis);
+    const client  = redis.createClient(6379, config.host_redis);
 
-client.on("connect",function(){
-  console.log("redis conected");
-
-});
-
-
-var sessionStore = new redisStore({
-      "client": client,
-      "host": config.host_redis,
-      "port": 6379
+    client.on('connect',function(){
+      console.log('redis conected');
     });
 
-var sessionSettings = {
-    secret: "my-secret",
-    resave: true,
-    saveUninitialized: true,
-    store: sessionStore,
-    /*cookie : {
-        expires: new Date(253402300000000),
-        maxAge: 253402300000000
-    }*/
 
-};
+    const sessionStore = new redisStore({
+      'client': client,
+      'host': config.host_redis,
+      'port': 6379
+    });
+
+    const sessionSettings = {
+      secret: 'my-secret',
+      resave: true,
+      saveUninitialized: true,
+      store: sessionStore,
+      /*cookie : {
+          expires: new Date(253402300000000),
+          maxAge: 253402300000000
+      }*/
+    };
 
 
-var socketSession = socketIOSession(sessionSettings);
+    const socketSession = socketIOSession(sessionSettings);
 
 
-app.use(session(sessionSettings));
-websocket_client.use(socketSession.parser);
+    app.use(session(sessionSettings));
+    websocket_client.use(socketSession.parser);
 
-}
+  },
 
 }
