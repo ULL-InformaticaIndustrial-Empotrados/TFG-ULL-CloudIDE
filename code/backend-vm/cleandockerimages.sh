94d26eff3767748f8b6845aff5a47bbd68a327b7
Alberto Hamilton Castro <alberto@isaatc.ull.es>
2018-08-23 17:37:37 +0100

    Quitamos `sudo` de los .sh

--- code/backend-vm/cleandockerimages.sh
+++ code/backend-vm/cleandockerimages.sh
@@ -1,4 +1,5 @@
+#!/bin/bash
 
-echo $1 | sudo -S su
-
-sudo docker rmi $(sudo docker images | awk '$1 !~ /(alpine)|(eclipse\/che$)|(eclipse\/che-action)|(eclipse\/che-test)|(eclipse\/che-server)|(eclipse\/che-init)|(eclipse\/che-dir)|(eclipse\/che-mount)|(eclipse\/che-ip)|(docker\/compose)/ {print $3}')
+docker rmi $(docker images | \
+  tail -n +2 | \
+  awk '$1 !~ /(alpine)|(eclipse\/che$)|(eclipse\/che-action)|(eclipse\/che-test)|(eclipse\/che-server)|(eclipse\/che-init)|(eclipse\/che-dir)|(eclipse\/che-mount)|(eclipse\/che-ip)|(docker\/compose)/ {print $3}')
--- code/backend-vm/comprobarche.sh
+++ code/backend-vm/comprobarche.sh
@@ -1,6 +1,7 @@
-echo $1 | sudo -S su
-sudo docker rm $(sudo docker ps -aq) #limpiamos ids de docker que hayan podido quedarse y que no estén ejecutandose
-existe=$(sudo docker ps -qf "name=ULLcloudIDE-$2")
+#!/bin/bash
+
+docker rm $(docker ps -aq) #limpiamos ids de docker que hayan podido quedarse y que no estén ejecutandose
+existe=$(docker ps -qf "name=ULLcloudIDE-$2")
 #echo ${#existe}
 if [ "${#existe}" = "0" ]; then
   echo "no existe";
