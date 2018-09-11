5f4bbd2ed852618450880e3fefac533868dcef67
Alberto Hamilton Castro <alberto@isaatc.ull.es>
2018-09-11 13:39:55 +0100

    Quitamos `cleardockerimages.sh`

--- code/backend-vm/cleandockerimages.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker rmi $(docker images | \
-  tail -n +2 | \
-  awk '$1 !~ /(alpine)|(eclipse\/che$)|(eclipse\/che-action)|(eclipse\/che-test)|(eclipse\/che-server)|(eclipse\/che-init)|(eclipse\/che-dir)|(eclipse\/che-mount)|(eclipse\/che-ip)|(docker\/compose)/ {print $3}')
--- code/backend-vm/functions.js
+++ code/backend-vm/functions.js
@@ -2,7 +2,7 @@ const logger = require('winston');
 
 const os = require('os');
 
-const { exec } = require('child_process');
+const { exec } = require('child-process-promise');
 
 const getiplocal = function () {
   const interfaces = os.networkInterfaces();
@@ -22,16 +22,26 @@ const getiplocal = function () {
 
 const cleandockerimages = function () {
 
-  const child = exec(__dirname + '/cleandockerimages.sh',
-    function (error, stdout, stderr) {
-
-      // controlamos el error
-      if (error !== null) {
-        logger.warn(`Error cleandockerimages: "${error}"`);
-      }
-
-      logger.debug(`cleandockerimages salida estandar: "${stdout}"`);
-    });
+  const comando = `/usr/bin/docker images --format "{{.ID}}:{{.Repository}}" \
+      | grep -v -f imagenesConservar.lst \
+      | cut -d: -f1 \
+      | uniq \
+      | tr '\n' ' ' \
+  `;
+  logger.debug(`cleandockerimages comando: "${comando}"`);
+  exec(comando)
+    .then((result) => {
+      if (result.stdout.length > 0) {
+        logger.debug(`Borrando imagenes docker "${result.stdout}"`);
+        return exec(`/usr/bin/docker rmi ${result.stdout}`);
+      } else
+        logger.debug(`No hay imagenes que borrar`);
+    })
+    .then((result) => {
+      if (result)  // puede no haberse ejecutado rmi
+        logger.debug(`Borrando imagenes salida : "${result.stdout}"`);
+    })
+    .catch((error) => logger.warn(`Error borrando images: "${error}"`));
 
 };
 
