7b4bc7c4d70aca04d962c51a0fc0caa8f912c9a9
Alberto Hamilton Castro <alberto@isaatc.ull.es>
2018-09-22 13:51:37 +0100

    Mejoramos mensajes.

--- code/portal-vm/aniadeProfesor.js
+++ code/portal-vm/aniadeProfesor.js
@@ -6,16 +6,11 @@ const connection = mysql.createConnection({
   user: config.user_bbdd_mysql,
   password: config.password_bbdd_mysql,
   database : config.database_bbdd_mysql,
-  //debug : true,
   acquireTimeout : 60 * 60 * 1000,
   connectTimeout : 60 * 60 * 1000,
   timeout : 60 * 60 * 1000,
   connectionLimit : 5,
   queueLimit : 0
-});
-
-process.argv.forEach((val, index) => {
-  console.log(`${index}: ${val}`);
 });
 
 if (process.argv.length < 3) {
@@ -26,9 +21,12 @@ if (process.argv.length < 3) {
   console.log(`Consulta de inserción: "${consulta}"`);
   connection.connect();
   connection.query(consulta, (error, result, fields) => {
-      console.log(`Error consulta: ${JSON.stringify(error, null, 2)}`);
-      console.log(`Resultado consulta: ${JSON.stringify(result, null, 2)}`);
-      console.log(`Fields consulta: ${JSON.stringify(fields, null, 2)}`);
-    });
+    if (error)
+      console.log(`ERROR consulta: ${JSON.stringify(error, null, 2)}`);
+    if (result)
+      console.log(`RESULTADO consulta: ${JSON.stringify(result, null, 2)}`);
+    if (fields)
+      console.log(`FIELD consulta: ${JSON.stringify(fields, null, 2)}`);
+  });
   connection.end();
 }
