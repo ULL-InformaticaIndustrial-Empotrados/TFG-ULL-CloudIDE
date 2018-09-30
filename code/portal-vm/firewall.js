d2edbcc2dff28f594192316c875cee0a0b9c4ed4
Alberto Hamilton Castro <alberto@isaatc.ull.es>
2018-09-30 12:53:42 +0100

    Arreglamos comillas, exec, etc.

--- code/portal-vm/firewall.js
+++ code/portal-vm/firewall.js
@@ -1,88 +1,81 @@
-const logger = require('winston');
-var config = require('./config.json');
+const logger = require(`winston`);
+const { exec } = require(`child-process`);
+
+
+const config = require(`./config.json`);
+
 module.exports = {
 
-  dnatae : function(modo, ip_source, ip_dest, port, callback){
-    var exec = require('child_process').exec, child;
+  dnatae(modo, ipSource, ipDest, port, callback) {
+    let modoI;
+    if (modo === `añadirtodo`) { // añadirtodo -> 1, eliminar -> 4, añadirsolo -> 3
+      logger.debug(`Añadir todo de la ip de origen "${ipSource}"`);
+      modoI = 1;
+    }
+    if (modo === `añadircomienzo`) {
+      logger.debug(`Añadir comienzo de la ip de origen "${ipSource}"`);
+      modoI = 2;
+    }
+    if (modo === `eliminarsolo`) {
+      logger.debug(`Eliminar solo de la ip de origen "${ipSource}"`);
+      modoI = 4;
+    }
+    if (modo === `añadirsolo`) {
+      logger.debug(`Añadir solo de la ip de origen "${ipSource}"`);
+      modoI = 3;
+    }
 
-     if(modo == "añadirtodo"){ //añadirtodo -> 1, eliminar -> 4, añadirsolo -> 3
-       logger.debug(`Añadir todo de la ip de origen "${ip_source}"`);
-       modo = 1;
-     }
-      if(modo == "añadircomienzo"){
-       logger.debug(`Añadir comienzo de la ip de origen "${ip_source}"`);
-       modo = 2;
-     }
-     if(modo == "eliminarsolo"){
-       logger.debug(`Eliminar solo de la ip de origen "${ip_source}"`);
-       modo = 4;
-     }
-      if(modo == "añadirsolo"){
-       logger.debug(`Añadir solo de la ip de origen "${ip_source}"`);
-       modo = 3;
-     }
-
-    child = exec('./sh/dnat.sh ' + modo + " " + ip_source +" "+ ip_dest +" "+ port+" "+config.password_root,
-      function (error, stdout, stderr) {
+    exec(`./sh/dnat.sh ` + modoI + ` ` + ipSource +` `+ ipDest +` `+ port+` `+config.password_root,
+      (error, stdout, stderr) => {
         // controlamos el error
         if (error !== null) {
           logger.error(`dnat.sh exec error: "${error}"`);
         }
         logger.debug(`dnat.sh exec salida: "${stdout}"`);
 
-        if(callback != null){
+        if (callback != null) {
           callback();
         }
       });
-
   },
 
+  deletednat(ipSource, callback) {
+    logger.debug(`Eliminar todo de la ip de origen "${ipSource}"`);
 
-  deletednat : function(ip_source, callback){
-    logger.debug(`Eliminar todo de la ip de origen "${ip_source}"`);
-    var exec = require('child_process').exec, child;
-
-    child = exec('./sh/deletednat0.sh '+ ip_source +" "+config.password_root,
-      function (error, stdout, stderr) {
+    exec(`./sh/deletednat0.sh `+ ipSource +` `+config.password_root,
+      (error, stdout, stderr) => {
         // controlamos el error
         if (error !== null) {
           logger.error(`deletednat0.sh exec error: "${error}"`);
         }
         logger.debug(`deletednat0.sh exec salida: "${stdout}"`);
 
-        if(callback != null){
+        if (callback != null) {
           callback();
         }
-    });
-
+      });
   },
 
-
-  inicializar : function(){
-    var exec = require('child_process').exec, child;
-
-    child = exec('./sh/inicializar.sh '+config.password_root +" "+ config.interfaz_exterior +" "+ config.interfaz_interior +" "+ config.ip_server_interior,
-      function (error, stdout, stderr) {
+  inicializar() {
+    exec(`./sh/inicializar.sh `+config.password_root +` `+ config.interfaz_exterior +` `+ config.interfaz_interior +` `+ config.ip_server_interior,
+      (error, stdout, stderr) => {
         // controlamos el error
         if (error !== null) {
           logger.error(`inicializar.sh exec error: "${error}"`);
         }
         logger.debug(`inicializar.sh exec salida: "${stdout}"`);
-    });
+      });
   },
 
-
-  tcpkillestablished : function(ip_source){
-    var exec = require('child_process').exec, child;
-
-    child = exec('./sh/tcpkillestablished.sh '+config.password_root +" "+ ip_source,
-      function (error, stdout, stderr) {
+  tcpkillestablished(ipSource) {
+    exec(`./sh/tcpkillestablished.sh `+config.password_root +` `+ ipSource,
+      (error, stdout, stderr) => {
         // controlamos el error
         if (error !== null) {
           logger.error(`tcpkillestablished.sh exec error: "${error}"`);
         }
         logger.debug(`tcpkillestablished.sh exec salida: "${stdout}"`);
-    });
-  }
+      });
+  },
 
-}
+};
