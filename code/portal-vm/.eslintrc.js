538c286fe8827d053fe89e60369db9c63e4da930
Alberto Hamilton Castro <albham@gmail.com>
2019-08-06 18:05:55 +0100

    Para eslint quitamos backtick y añadimios otras reglas útiles.

--- code/portal-vm/.eslintrc.js
+++ code/portal-vm/.eslintrc.js
@@ -1,10 +1,15 @@
 module.exports = {
-    "extends": "airbnb-base",
-    "rules": {
-        // prefiere backtick, pero solo warning
-        "quotes": ["warn", "backtick", { "avoidEscape": true }],
-        // para que los then de las promesas puedan estar al mismo nivel
-        'indent': ['error', 2, { 'MemberExpression': 'off'}]
-    }
+  extends: 'airbnb-base',
+  rules: {
+    // prefiere backtick, pero solo warning
+    // "quotes": ["warn", "backtick", { "avoidEscape": true }],
+    // para que los then de las promesas puedan estar al mismo nivel
+    indent: ['error', 2, { MemberExpression: 'off' }],
+    // Permitimos los subrayados
+    'no-underscore-dangle': 'off',
+    'no-prototype-builtins': 'off',
+    'no-restricted-syntax': ['off', 'ForStatement'],
+    'no-trailing-spaces': 'off', // editor se encarga de quitarlos
+  },
 
 };
