5bd1728b6c0f419fac0eff0ba73d8dc9d6755cc9
Alberto Hamilton Castro <albham@gmail.com>
2019-08-09 09:26:13 +0100

    Igualamos los eslintrc

--- code/backend-vm/.eslintrc.js
+++ code/backend-vm/.eslintrc.js
@@ -1,12 +1,14 @@
 module.exports = {
-    "extends": "airbnb-base",
-    "rules": {
-        // prefiere backtick, pero solo warning
-        "quotes": ["warn", "backtick", { "avoidEscape": true }],
-        // para que los then de las promesas puedan estar al mismo nivel
-        'indent': ['error', 2, { 'MemberExpression': 'off'}],
-        'no-restricted-syntax': ['off', 'ForStatement'],
-        'no-trailing-spaces': 'off', // editor se encarga de quitarlos
-    }
-
+  extends: 'airbnb-base',
+  rules: {
+    // prefiere backtick, pero solo warning
+    //'quotes': ['warn', 'backtick', { 'avoidEscape': true }],
+    // para que los then de las promesas puedan estar al mismo nivel
+    'indent': ['error', 2, { 'MemberExpression': 'off'}],
+    // Permitimos los subrayados
+    'no-underscore-dangle': 'off',
+    'no-prototype-builtins': 'off',
+    'no-restricted-syntax': ['off', 'ForStatement'],
+    'no-trailing-spaces': 'off', // editor se encarga de quitarlos
+  }
 };
