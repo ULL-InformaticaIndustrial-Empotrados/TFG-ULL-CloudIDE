d1f38a40e991b49032d5ad631c95fbcad9ea48bd
Alberto Hamilton Castro <albham@gmail.com>
2019-08-16 10:20:44 +0100

    Actulizamos `restauraservicio.js`

--- code/portal-vm/database.js
+++ code/portal-vm/database.js
@@ -3,23 +3,28 @@ const CREDS = require('./creds');
 const config = require('./config.json');
 const logger = require('./logger.js');
 
-const pool = mysql.createPool({
-  host: config.host_bbdd_mysql,
-  user: CREDS.user_bbdd_mysql,
-  password: CREDS.password_bbdd_mysql,
-  database: config.database_bbdd_mysql,
-  // debug : true,
-  acquireTimeout: 60 * 60 * 1000,
-  connectTimeout: 60 * 60 * 1000,
-  timeout: 60 * 60 * 1000,
-  connectionLimit: 5,
-  queueLimit: 0,
-});
+module.exports = {
+  async creaPool() {
+    const pool = await mysql.createPool({
+      host: config.host_bbdd_mysql,
+      user: CREDS.user_bbdd_mysql,
+      password: CREDS.password_bbdd_mysql,
+      database: config.database_bbdd_mysql,
+      // debug : true,
+      acquireTimeout: 60 * 60 * 1000,
+      connectTimeout: 60 * 60 * 1000,
+      timeout: 60 * 60 * 1000,
+      connectionLimit: 5,
+      queueLimit: 0,
+    });
 
-logger.debug('creado Pool conexiones MySQL');
+    logger.debug('creado Pool conexiones MySQL');
 
-pool.on('release', (connection) => {
-  logger.debug(`Connection MySQL "${connection.threadId}" released`);
-});
+    pool.on('release', (connection) => {
+      logger.debug(`Connection MySQL "${connection.threadId}" released`);
+    });
 
-module.export = pool;
+    return pool;
+  },
+
+};
--- code/portal-vm/restaurarservicio.js
+++ code/portal-vm/restaurarservicio.js
@@ -1,60 +1,65 @@
 
-var functions = require('./functions.js');
-var config = require('./config.json');
-var PythonShell = require('python-shell');
+const PythonShell = require('python-shell');
+const db = require('./database.js');
+const logger = require('./logger.js');
 
+logger.info(`Tenemos db: ${JSON.stringify(db, null, 2)}`);
 
-var pool = functions.createnewconnection();
+async function restaura() {
+  const pool = await db.creaPool();
+  // logger.info(`Tenemos pool: ${pool.constructor.name}`);
+  logger.info(`Tenemos pool: ${pool.constructor.name}`);
+  // console.log(`Tenemos pool: ${JSON.stringify(pool, null, 2)}`);
+  const connection = await pool.getConnection();
+  logger.info(`Tenemos connection: ${connection.constructor.name}`);
+  let sentencia;
+  sentencia = 'TRUNCATE Cola';
+  await connection.query(sentencia);
+  logger.info(sentencia);
+  sentencia = 'TRUNCATE Firewall';
+  await connection.query(sentencia);
+  logger.info(sentencia);
+  sentencia = 'TRUNCATE Pendientes';
+  await connection.query(sentencia);
+  logger.info(sentencia);
+  sentencia = 'TRUNCATE VMS';
+  await connection.query(sentencia);
+  logger.info(sentencia);
+  sentencia = 'TRUNCATE Asignaciones';
+  await connection.query(sentencia);
+  logger.info(sentencia);
+  sentencia = 'TRUNCATE Servidores';
+  await connection.query(sentencia);
+  logger.info(sentencia);
+  sentencia = 'TRUNCATE Ovirt';
+  await connection.query(sentencia);
+  logger.info(sentencia);
+  sentencia = 'TRUNCATE Eliminar_servicio';
+  await connection.query(sentencia);
+  logger.info(sentencia);
+  sentencia = 'TRUNCATE Eliminar_servicio_usuario';
+  await connection.query(sentencia);
+  logger.info(sentencia);
+  sentencia = 'TRUNCATE Ovirt_Pendientes';
+  await connection.query(sentencia);
+  logger.info(sentencia);
+  sentencia = 'TRUNCATE Ovirt_Pendientes_Up_AddStart';
+  await connection.query(sentencia);
+  logger.info(sentencia);
+  logger.info('empty BBDD');
+  connection.release();
 
+  const options = {
+    mode: 'text',
+    scriptPath: './ovirtpython',
+  };
 
-    pool.getConnection(function(err, connection) {
+  PythonShell.run('stop_and_remove_all_vm.py', options, (err, results) => {
+    if (err) throw err;
+    // results is an array consisting of messages collected during execution
+    logger.info(`results: ${results}`);
+    process.exit();
+  });
+}
 
-      connection.query('TRUNCATE Cola', function(error, results, fields) {
-        console.log(".");
-        connection.query('TRUNCATE Firewall', function(error, results, fields) {
-          console.log("..");
-          connection.query('TRUNCATE Pendientes', function(error, results, fields) {
-            console.log("...");
-            connection.query('TRUNCATE VMS', function(error, results, fields) {
-              console.log("....");
-              connection.query('TRUNCATE Asignaciones', function(error, results, fields) {
-                console.log(".....");
-                  connection.query('TRUNCATE Servidores', function(error, results, fields) {
-                    console.log("......");
-                      connection.query('TRUNCATE Ovirt', function(error, results, fields) {
-                        console.log(".......");
-                        connection.query('TRUNCATE Eliminar_servicio', function(error, results, fields) {
-                          console.log(".........");
-                          connection.query('TRUNCATE Eliminar_servicio_usuario', function(error, results, fields) {
-                            console.log("..........");
-                              connection.query('TRUNCATE Ovirt_Pendientes', function(error, results, fields) {
-                                console.log("............");
-                                  connection.query('TRUNCATE Ovirt_Pendientes_Up_AddStart', function(error, results, fields) {
-                                    console.log("empty BBDD");
-                                    connection.release();
-
-                                    var options = {
-                                      mode: 'text',
-                                      scriptPath: './ovirtpython'
-                                    };
-
-                                    PythonShell.run('stop_and_remove_all_vm.py', options, function (err, results) {
-                                      if (err) throw err;
-                                      // results is an array consisting of messages collected during execution
-                                      console.log('results: %j', results);
-                                      process.exit();
-
-                                      });
-                                  });
-                                });
-                          });
-                      });
-                  });
-                });
-              });
-            });
-          });
-        });
-      });
-
-    });
+restaura();
