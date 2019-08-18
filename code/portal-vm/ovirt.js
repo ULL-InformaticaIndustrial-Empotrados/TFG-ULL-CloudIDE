94e6c97eb268ef0cb72b36e3bb53f705f7bcd6a5
Alberto Hamilton Castro <albham@gmail.com>
2019-08-18 18:31:56 +0100

    Actualizamos `ovirt.js`

--- code/portal-vm/ovirt.js
+++ code/portal-vm/ovirt.js
@@ -1,42 +1,37 @@
-const logger = require('winston');
-var os = require('os');
-var config = require('./config.json');
-var PythonShell = require('python-shell');
+const PythonShell = require('python-shell');
+
+const logger = require('./logger.js').child({ label: 'ovirt' });
 
 module.exports = {
 
-    add_and_start_vm : function(name, ip_address, callback){
+  add_and_start_vm(name, ipAddress, callback) {
+    const options = {
+      mode: 'text',
+      scriptPath: './ovirtpython',
+      args: [name, ipAddress],
+    };
 
-      var options = {
-        mode: 'text',
-        scriptPath: './ovirtpython',
-        args: [name, ip_address]
-      };
+    PythonShell.run('add_and_start_vm.py', options, (err, results) => {
+      if (err) throw err;
+      // results is an array consisting of messages collected during execution
+      logger.debug(`add_and_start_vm.py results: "${results}"`);
+      callback();
+    });
+  },
 
-      PythonShell.run('add_and_start_vm.py', options, function (err, results) {
-        if (err) throw err;
-        // results is an array consisting of messages collected during execution
-        logger.debug(`add_and_start_vm.py results: "${results}"`);
-        callback();
-      });
+  stop_and_remove_vm(name, callback) {
+    const options = {
+      mode: 'text',
+      scriptPath: './ovirtpython',
+      args: [name],
+    };
 
-    },
+    PythonShell.run('stop_and_remove_vm.py', options, (err, results) => {
+      if (err) throw err;
+      // results is an array consisting of messages collected during execution
+      logger.debug(`stop_and_remove_vm.py results: "${results}"`);
+      callback();
+    });
+  },
 
-    stop_and_remove_vm : function(name, callback){
-
-      var options = {
-        mode: 'text',
-        scriptPath: './ovirtpython',
-        args: [name]
-      };
-
-      PythonShell.run('stop_and_remove_vm.py', options, function (err, results) {
-        if (err) throw err;
-        // results is an array consisting of messages collected during execution
-        logger.debug(`stop_and_remove_vm.py results: "${results}"`);
-        callback();
-      });
-
-    }
-
-}
+};
