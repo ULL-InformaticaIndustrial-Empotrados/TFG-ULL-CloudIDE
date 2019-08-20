f65d43dfed78dbdc315a7a23d057fb80c9a09657
Alberto Hamilton Castro <albham@gmail.com>
2019-08-20 12:46:47 +0100

    `pruebaSQL.js` permite varias sentencias en misma conección.

---
+++ code/portal-vm/pruebas/pruebaSQL.js
@@ -0,0 +1,30 @@
+const logger = require('../logger.js').child({ label: 'pruebaSQL' });
+
+logger.info('Comienza pruebaSQL');
+
+const db = require('../database.js');
+
+async function main() {
+  if (process.argv.length < 3) {
+    logger.error('No se ha pasado sentencia SQL');
+  } else {
+    const pool = await db.pool;
+    const connection = await pool.getConnection();
+    let argAct = 2;
+    while (argAct < process.argv.length) {
+      const sentencia = process.argv[argAct];
+      logger.info(`Ejecutando consulta SQL: "${sentencia}"`);
+      try {
+        const result = await connection.query(sentencia);
+        logger.info(`RESULTADO consulta: ${JSON.stringify(result, null, 2)}`);
+      } catch (error) {
+        logger.error(`ERROR consulta: ${JSON.stringify(error, null, 2)}`);
+      }
+      argAct += 1;
+    }
+    await connection.release();
+    await pool.end();
+  }
+}
+
+main();
