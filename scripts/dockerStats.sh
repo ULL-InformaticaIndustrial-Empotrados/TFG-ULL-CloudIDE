9dfe919858882b633582e563389d15dd696113be
Alberto Hamilton Castro <albham@gmail.com>
2019-09-13 16:09:31 +0100

    Movemos dockerStats.sh

--- scripts/dockerStats.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# dockerStats.sh
-(docker stats --no-stream --format "container:{{ .Container }}, name:{{ .Name }}, memoryRaw:{{ .MemUsage }}, memoryPercent:{{ .MemPerc }}, cpu:{{ .CPUPerc }}" && df -hT /dev/mapper/ubuntu--vg-root | sed -e /S.ficheros/d | awk '{ printf gensub(",", ".", 1) }'| awk '{ print "total:" $3, "used:" $4, "available:" $5, $8}') >> /var/log/dockerStats.log
-sleep 5;
-# ---
-
