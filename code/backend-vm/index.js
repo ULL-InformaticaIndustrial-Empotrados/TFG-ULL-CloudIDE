5345776ac1305820277a649dd0d37240a6e819e3
Alberto Hamilton Castro <albham@gmail.com>
2020-06-08 20:04:33 +0100

    Correción eslint.

--- code/backend-vm/index.js
+++ code/backend-vm/index.js
@@ -94,9 +94,8 @@ async function arrancaChe(user, motivo, port) {
     logger.debug(`Arranque contenedor salida estandar: "${result.stdout}"`);
     correcto = true;
   } catch (error) {
-    logger.warn(`Error Arranque contenedor: "${error}"`, {
-      user, motivo, ipVM, port,
-    });
+    logger.warn(`Error Arranque contenedor: "${error}"`,
+      { user, motivo, ipVM, port });
   }
   return correcto;
 }
