af0de239bd543843e166ad30886a533a790f40a8
Alberto Hamilton Castro <alberto@isaatc.ull.es>
2020-06-16 12:46:23 +0100

    Mejoramos presentación info BD

--- scripts/estadoBD.sh
+++ scripts/estadoBD.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-mysql -c -t -u usuario --password=usuario cloudIDE <<__EOF__
-select "Asignaciones";
-select * from Asignaciones;
-select "Cola";
-select * from Cola;
-select "Pendientes";
-select * from Pendientes;
-select "Ovirt";
-select * from Ovirt;
-select "VMS";
-select * from VMS;
-select "Ovirt_Pendientes";
-select * from Ovirt_Pendientes;
-select "Ovirt_Pendientes_up_addstart";
-select * from Ovirt_Pendientes_Up_AddStart;
-__EOF__
+CMD="mysql -c -t -u usuario --password=usuario cloudIDE"
+echo "Asignaciones ===================="
+$CMD -e 'select * from Asignaciones;' 2>/dev/null
+echo "Cola ============================"
+$CMD -e 'select * from Cola;' 2>/dev/null
+echo "Pendientes ======================"
+$CMD -e 'select * from Pendientes;' 2>/dev/null
+echo "Ovirt ==========================="
+$CMD -e 'select * from Ovirt;' 2>/dev/null
+echo "VMS ============================"
+$CMD -e 'select * from VMS;' 2>/dev/null
+echo "Ovirt_Pendientes ==============="
+$CMD -e 'select * from Ovirt_Pendientes;' 2>/dev/null
+echo "Ovirt_Pendientes_up_addstart ==="
+$CMD -e 'select * from Ovirt_Pendientes_Up_AddStart;' 2>/dev/null
